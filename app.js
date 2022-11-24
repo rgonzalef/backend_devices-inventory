131c82fa67b9d98e454d180605e8284429303627
Raúl Gonzalez <rgonzalef@gmail.com>
2022-11-23 23:03:46 -0500

    feat:add mongo app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')// se llama a la libreria mogoose
 
 require("dotenv").config();
 
@@ -21,8 +22,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//se agrega para verificar la conexión con la BD
+mongoose
+  .connect(process.env.URI)
+  .then(() => console.log('Your database is connected'))
+  .catch((err)=> console.log(err));
 
+
+
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
